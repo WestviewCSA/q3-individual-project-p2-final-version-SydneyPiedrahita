ad4a4cb52397f945a9f46f0cfb37d9b983ac2f03
sydneypiedrahita <sydneypiedrahita@localhost>
2026-03-16 18:48:03 -0700

    findStart and findGoal

--- Q3 Project 1 Period 2/src/Solver.java
+++ Q3 Project 1 Period 2/src/Solver.java
@@ -6,11 +6,39 @@ public class Solver {
 	private int col;
 	
 	
-	public Solver() {
-		
+	public Solver(String[][] map) {
+	    this.map = map;
+	    
+	}
+	
+	public int[] findStart() {
+		for(int row = 0; row < map.length; row++) {
+			for(int col = 0; col < map[0].length; col++) {
+				if(map[row][col].equals("W")) {
+					return new int[] {row, col};
+				}
+			}
+		}
+		//if not found:
+		return null;
+	}
+	
+	public int[] findGoal() {
+		for(int row = 0; row < map.length; row++) {
+			for(int col = 0; col < map[0].length; col++) {
+				if(map[row][col].equals("$")) {
+					return new int[] {row, col};
+				}
+			}
+		}
+		//if not found:
+		return null;
 	}
 	
 	public void Queue() {
+		int rows = map.length;
+		int cols = map[0].length;
+		Queue enqueue = new Queue;
 		
 	}
 	
