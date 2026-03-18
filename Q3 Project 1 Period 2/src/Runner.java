cc3445d5cba64f18161cbc0a7f5a8009f98f1143
sydneypiedrahita <sydneypiedrahita@localhost>
2026-03-17 18:58:33 -0700

    im an idiot and suck at coding so much its all wrong starting over

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -8,13 +8,7 @@ public class Runner {
 		String[][] map = m.readFile("easyMap2");
 		Solver s = new Solver(map);
 		s.Queue();
-		System.out.println(s.findStart());
 		s.printMap();
-		Queue<int[]> path = s.Queue();
-		while(!path.isEmpty()) {
-			int[] cell = path.remove();
-			System.out.println("[" + cell[0] + ", " + cell[1] + "]");
-		}
 	}
 
 
--- Q3 Project 1 Period 2/src/Solver.java
+++ Q3 Project 1 Period 2/src/Solver.java
@@ -37,17 +37,42 @@ public class Solver {
 		return null;
 	}
 	
-	public Queue<int[]> Queue() {
+	public void Queue() {
 		//FIFO
 		int rows = map.length;
 		int cols = map[0].length;
 		int[] start = findStart();
 		int[] goal = findGoal();
 		
+		 
+        // safety checks first
+        if (start == null) {
+            System.out.println("Can't find Start");
+            return;
+        }
+        if (goal == null) {
+            System.out.println("Can't find Start");
+            return;
+        }
 
-		Queue<int[]> enQueue = new LinkedList<>();
+    
+        int[][][] cameFrom = new int[rows][cols][];
+
+        boolean[][] visited = new boolean[rows][cols];
+
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(start);
+        visited[start[0]][start[1]] = true;
+
+
+    }
+
+
+		/*Queue<int[]> enQueue = new LinkedList<>();
 		Queue<int[]> deQueue = new LinkedList<>();
 		enQueue.add(start);
+		deQueue.add(start);
 		
 		
 		
@@ -63,7 +88,7 @@ public class Solver {
 		//map[start[0]][start[1]] = "+";
 		int[] curr = start;
 		
-		while(curr[0] != goal[0] && curr[1] != goal[1]) {
+		while(curr[0] != goal[0] || curr[1] != goal[1]) {
 			int[] N = {curr[0]-1, curr[1]};
 			int[] S = {curr[0]+1, curr[1]};
 			int[] E =  {curr[0], curr[1]+1};
@@ -71,49 +96,46 @@ public class Solver {
 			
 			if(N[0] == goal[0] && N[1] == goal[1]) {
 				enQueue.add(N);
-				map[N[0]][N[1]] = "+";
+				//map[N[0]][N[1]] = "%";
 				return enQueue;
 			}
 			//N[0] != goal[0] && N[1] != goal[1]
-			else if(N[0] >= 0 && N[1] >= 0 && !map[N[0]][N[1]].equals("+") && !map[N[0]][N[1]].equals("#")) {
+			else if(N[0] >= 0 && N[1] >= 0 && !map[N[0]][N[1]].equals("+") && !map[N[0]][N[1]].equals("@")&& !map[N[0]][N[1]].equals("W") ) {
 				deQueue.add(N);
-				//map[N[0]][N[1]] = "+";
+				map[N[0]][N[1]] = "+";
 			}
 			
 			if(S[0] == goal[0] && S[1] == goal[1]) {
 				enQueue.add(S);
-				map[S[0]][S[1]] = "+";
+				//map[S[0]][S[1]] = "%";
 				return enQueue;
 			}
-			else if(S[0] >= 0 && S[1] >= 0 && !map[S[0]][N[1]].equals("+") && !map[S[0]][S[1]].equals("#")) {
+			else if(S[0] >= 0 && S[1] >= 0 && !map[S[0]][S[1]].equals("+") && !map[S[0]][S[1]].equals("@") && !map[S[0]][S[1]].equals("W") ) {
 				deQueue.add(S);
-				//map[S[0]][S[1]] = "+";
+				map[S[0]][S[1]] = "+";
 			}
 			if(E[0] == goal[0] && E[1] == goal[1]) {
 				enQueue.add(E);
-				map[E[0]][E[1]] = "+";
+				//map[E[0]][E[1]] = "%";
 				return enQueue;
 			}
-			else if(E[0] >= 0 && E[1] >= 0 && !map[E[0]][E[1]].equals("+") && !map[E[0]][E[1]].equals("#")) {
+			else if(E[0] >= 0 && E[1] >= 0 && !map[E[0]][E[1]].equals("+") && !map[E[0]][E[1]].equals("@") && !map[E[0]][E[1]].equals("W") ) {
 				deQueue.add(E);
-				//map[E[0]][E[1]] = "+";
+				map[E[0]][E[1]] = "+";
 			}
 			if(W[0] == goal[0] && W[1] == goal[1]) {
 				enQueue.add(W);
-				map[W[0]][W[1]] = "+";
+				//map[W[0]][W[1]] = "%";
 				return enQueue;
 			}
-			else if(W[0] >= 0 && W[1] >= 0 && !map[W[0]][W[1]].equals("+") && !map[W[0]][W[1]].equals("#")) {
+			else if(W[0] >= 0 && W[1] >= 0 && !map[W[0]][W[1]].equals("+") && !map[W[0]][W[1]].equals("@") && !map[W[0]][W[1]].equals("W") ) {
 				deQueue.add(W);
-				//map[W[0]][W[1]] = "+";
+				map[W[0]][W[1]] = "+";
 			}
+			
 			
 			
 			curr = deQueue.remove();
-			
-			while(map[curr[0]][curr[1]].equals("+") && !deQueue.isEmpty()) {
-				curr = deQueue.remove();
-			}
 			
 			enQueue.add(curr);
 			
@@ -121,9 +143,11 @@ public class Solver {
 				System.out.println("no path");
 				return enQueue;
 			}
+			
 		}
 		return enQueue;
-	}
+		*/
+		
 	
 	public void printMap() {
 		for(int row = 0; row < map.length; row++) {
